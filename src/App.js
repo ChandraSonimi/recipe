a62392b4481911d60b58d6f09a85228a67aa01e3
ChandraSonimi <sonimichandra123@gmail.com>
2023-03-19 16:23:54 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+ import Comp1 from './Comp1'
+ import 'bootstrap/dist/css/bootstrap.min.css'
+ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+function App() {
+  return (
+    <div>
+      <Comp1/>
+      
+    </div>
+  )
+}
+
+export default App
